94aed5dae71614673600da07aada6d0c6c88dcee
kris-i-kris <flare120491@gmail.com>
2024-04-17 22:46:10 +0700

    Docs

--- src/test/java/ru/netology/buytour/tests/DBTest.java
+++ src/test/java/ru/netology/buytour/tests/DBTest.java
@@ -1,33 +1,20 @@
 package ru.netology.buytour.tests;
 
-import com.codeborne.selenide.Configuration;
+
 import com.codeborne.selenide.logevents.SelenideLogger;
 import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.*;
-import org.openqa.selenium.chrome.ChromeOptions;
 import ru.netology.buytour.data.DBHelper;
 import ru.netology.buytour.data.DataHelper;
 import ru.netology.buytour.page.PageHome;
 
-import java.util.HashMap;
-import java.util.Map;
-
 import static com.codeborne.selenide.Selenide.open;
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNull;
 import static ru.netology.buytour.data.DBHelper.getCreditId;
 
 public class DBTest {
     @BeforeEach
     void setup() {
-        ChromeOptions options = new ChromeOptions();
-        options.addArguments("--start-maximized");
-        Map<String, Object> prefs = new HashMap<String, Object>();
-        prefs.put("credentials_enable_service", false);
-        prefs.put("password_manager_enabled", false);
-        options.setExperimentalOption("prefs", prefs);
-        Configuration.browserCapabilities = options;
-
         open("http://localhost:8080");
     }
 
@@ -48,7 +35,7 @@ public class DBTest {
         var debitPage = new PageHome().buyDebit();
         debitPage.cardInfo(validCardInfo);
         debitPage.approvedMassage();
-        assertEquals("APPROVED", DBHelper.getPaymentStatus());
+        Assertions.assertEquals("APPROVED", DBHelper.getPaymentStatus());
 //        assertEquals(45000, DBHelper.getPaymentAmount());
     }
 
@@ -59,7 +46,7 @@ public class DBTest {
         var debitPage = new PageHome().buyDebit();
         debitPage.cardInfo(invalidCardInfo);
         debitPage.declinedMassage();
-        assertEquals("DECLINED", DBHelper.getPaymentStatus());
+        Assertions.assertEquals("DECLINED", DBHelper.getPaymentStatus());
     }
 
     @Test
@@ -69,8 +56,8 @@ public class DBTest {
         var debitPage = new PageHome().buyDebit();
         debitPage.cardInfo(validCardInfo);
         debitPage.approvedMassage();
-        assertEquals("APPROVED", DBHelper.getCreditRequestStatus());
-        assertNull(getCreditId());
+        Assertions.assertEquals("APPROVED", DBHelper.getCreditRequestStatus());
+        Assertions.assertNull(getCreditId());
     }
 
     @Test
@@ -80,7 +67,7 @@ public class DBTest {
         var debitPage = new PageHome().buyDebit();
         debitPage.cardInfo(invalidCardInfo);
         debitPage.declinedMassage();
-        assertEquals("DECLINED", DBHelper.getCreditRequestStatus());
-        assertNull(getCreditId());
+        Assertions.assertEquals("DECLINED", DBHelper.getCreditRequestStatus());
+        Assertions.assertNull(getCreditId());
     }
 }
